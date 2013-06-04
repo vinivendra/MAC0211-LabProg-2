3848c600e94597940a09c1f606b21414985e37a8
Vinicius Vendramini <vinivendra@gmail.com>
2013-06-04 15:56:32 -0300

    Temos quadrados

--- Canoa2/PrintAllegro.c
+++ Canoa2/PrintAllegro.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <allegro5/allegro.h>
 #include "allegro5/allegro_native_dialog.h"
+#include <allegro5/allegro_primitives.h>
 #include "Output.h"
 #include "rio.h"
 #include "pixel.h"
@@ -24,8 +25,12 @@ void outputArray (pixel **array, int altura, int largura, int indice) {
 void outputLine (pixel *linha, int largura) {
     color *= 1.01;
     al_clear_to_color(al_map_rgb_f(color, color, 0));
-    al_flip_display();
     
+    ALLEGRO_COLOR bla = al_map_rgb(0, 0, 0);
+    
+    al_draw_filled_rectangle(100, 100, 200, 200, bla);
+    
+    al_flip_display();
 }
 
 void clearScreen () {
