00e29a4e614b74c62f69645132f27a0ca1b3e042
Vinicius Vendramini <vinivendra@gmail.com>
2013-06-04 15:12:11 -0300

    Implementacao passada pro main

--- Canoa2/PrintAllegro.c
+++ Canoa2/PrintAllegro.c
@@ -25,7 +25,6 @@ float color = 0.1;
  Protótipos
 */
 
-void freeOutput ();
 
 /*
  Implementação
@@ -33,63 +32,12 @@ void freeOutput ();
 
 void initOutput (float velocidadeDoBarco) {
     
-    if(!al_init()) {        /* Inicializa o allegro. Se falhar, imprime o erro e sai. */
-        al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
-        exit(-1);
-    }
-    
-    display = al_create_display(640, 480);      /* Cria o display */
-    
-    if(!display) {          /* Caso haja erro na criação, imprime e sai. */
-        al_show_native_message_box(display, "Error", "Error", "Failed to create display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
-        exit(-1);
-    }
-    
-    event_queue = al_create_event_queue();
-    if(!event_queue) {
-        al_show_native_message_box(display, "Error", "Error", "Failed to create display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
-        freeOutput();
-        exit(-1);
-    }
-    
-    timer = al_create_timer(1.0/velocidadeDoBarco);
-    if(!timer) {
-        al_show_native_message_box(display, "Error", "Error", "Failed to create timer!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
-        freeOutput();
-        exit(-1);
-    }
-    
-    /* Registrar quaisquer fontes de eventos */
-    al_register_event_source(event_queue, al_get_display_event_source(display));
-    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
     
 }
 
 void startOutput (pixel **array, int altura, int largura, int indice) {
-    al_start_timer(timer);
-    
-    bool doexit = NO;
-    
-    outputArray(array, altura, largura, indice);
-    
-    while (!doexit) {
-        ALLEGRO_EVENT ev;
-        al_wait_for_event(event_queue, &ev);    /* Tells allegro to wait until an event comes from the queue "event_queue". */
-        
-        if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            doexit = YES;
-        }
-        
-        else if (ev.type == ALLEGRO_EVENT_TIMER && al_is_event_queue_empty(event_queue)) {   /* If the event was the timer reaching the instant for another loop */
-            
-            outputArray(array, altura, largura, indice);
-        }
-        /* According to the wiki, we only want to redraw the frame if the event queue is completely empty.
-         "Otherwise, the update loop could fall very far behind".*/
-        
-    }
-    
-    freeOutput();
+
 }
 
 void outputArray (pixel **array, int altura, int largura, int indice) {
@@ -103,15 +51,6 @@ void outputLine (pixel *linha, int largura) {
     
 }
 
-void freeOutput () {
-    if (display != NULL)
-        al_destroy_display(display);            /* Dá free no display */
-    if (event_queue != NULL)
-        al_destroy_event_queue(event_queue);    /* Dá free na event queue */
-    if (timer != NULL) {
-        al_destroy_timer(timer);
-    }
-}
 
 void clearScreen () {
     
--- Canoa2/main.c
+++ Canoa2/main.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <string.h>
 #include <allegro5/allegro.h>
+#include "allegro5/allegro_native_dialog.h"
 #include "Output.h"
 #include "grade.h"
 #include "util.h"
@@ -16,6 +17,17 @@
 #define distanciaEntreIlhasInicial 10
 #define probabilidadeDeObstaculosInicial 0.5
 #define limiteDasMargens 0.9
+
+#define YES 1
+#define NO 0
+typedef int BOOL;
+
+
+static ALLEGRO_DISPLAY *display = NULL;    /* Display, ou seja, a janela criada pelo allegro */
+static ALLEGRO_EVENT_QUEUE *event_queue = NULL;    /* A event queue, usada para manejar eventos */
+static ALLEGRO_TIMER *timer = NULL; /* O timer do programa */
+
+void freeOutput ();
 
 int main (int argc, char *argv[]) {
     
@@ -92,20 +104,67 @@ int main (int argc, char *argv[]) {
     clearScreen();
     
     /*
+     Allegro initialization
+    */
+    
+    if(!al_init()) {        /* Inicializa o allegro. Se falhar, imprime o erro e sai. */
+        al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        exit(-1);
+    }
+    
+    display = al_create_display(640, 480);      /* Cria o display */
+    
+    if(!display) {          /* Caso haja erro na criação, imprime e sai. */
+        al_show_native_message_box(display, "Error", "Error", "Failed to create display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        exit(-1);
+    }
+    
+    event_queue = al_create_event_queue();
+    if(!event_queue) {
+        al_show_native_message_box(display, "Error", "Error", "Failed to create display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        freeOutput();
+        exit(-1);
+    }
+    
+    timer = al_create_timer(1.0/velocidadeDoBarco);
+    if(!timer) {
+        al_show_native_message_box(display, "Error", "Error", "Failed to create timer!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+        freeOutput();
+        exit(-1);
+    }
+    
+    /* Registrar quaisquer fontes de eventos */
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    
+    /*
      Frames subsequentes
      */
     
-    /*for(;;){
-        indice = (indice - 1+alturaDaGrade) % alturaDaGrade;
+    al_start_timer(timer);
+    
+    bool doexit = NO;
+    
+    outputArray(grade, alturaDaGrade, larguraDoRio, indice);
+    
+    while (!doexit) {
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue, &ev);    /* Tells allegro to wait until an event comes from the queue "event_queue". */
         
-        criaProximoFrame(grade, alturaDaGrade, larguraDoRio, limiteMargens, fluxoDesejado, indice, dIlha, pIlha);
+        if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+            doexit = YES;
+        }
         
-        outputArray(grade, alturaDaGrade, larguraDoRio, indice);
+        else if (ev.type == ALLEGRO_EVENT_TIMER && al_is_event_queue_empty(event_queue)) {   /* If the event was the timer reaching the instant for another loop */
+            
+            outputArray(grade, alturaDaGrade, larguraDoRio, indice);
+        }
+        /* According to the wiki, we only want to redraw the frame if the event queue is completely empty.
+         "Otherwise, the update loop could fall very far behind".*/
         
-        clearScreen();
-        
-        nanosleep(&tim, &tim2);
-    }*/
+    }
+    
+    freeOutput();
     
     /*
      Frees
@@ -118,5 +177,14 @@ int main (int argc, char *argv[]) {
 
 
 
+void freeOutput () {
+    if (display != NULL)
+        al_destroy_display(display);            /* Dá free no display */
+    if (event_queue != NULL)
+        al_destroy_event_queue(event_queue);    /* Dá free na event queue */
+    if (timer != NULL) {
+        al_destroy_timer(timer);
+    }
+}
 
 
